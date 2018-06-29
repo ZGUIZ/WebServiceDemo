9d17e9c4c08436e17f9a82bd7e95071fbd7792b7
zguiz <rftoona@outlook.com>
2018-06-29 11:22:46 +0800

    注册对象序列化

--- src/com/zguiz/service/impl/UserServiceImpl.java
+++ src/com/zguiz/service/impl/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.zguiz.service.impl;
 import com.zguiz.bean.User;
 import com.zguiz.service.IUserService;
 
+import javax.xml.namespace.QName;
 import java.util.Date;
 
 public class UserServiceImpl implements IUserService{
     @Override
     public User login(String account, String password) {
+        System.out.println(account+"\t"+password);
         if("admin".equals(account)&&"admin".equals(password)){
             return new User(account,password,21,new Date(),true);
         }
